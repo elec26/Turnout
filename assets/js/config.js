7716c3a00defcdc6aa7b128730ecbedafa88a111
elec26 <y2n69mg97z@privaterelay.appleid.com>
2026-09-23 13:56:06 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 /* PARAMÈTRES PRINCIPAUX — modifiez ces valeurs si nécessaire. */
 window.SITE_CONFIG = {
-  participationRate: 11.34,
+  participationRate: 12.50,
   inscrits: 15801162,
   votersMode: "auto",
   electionDate: "2026-09-23",
